e8abe67913389e0ff3491dfb6e797a71388a68ec
Florian Bussmann <hallo@florian-bussmann.de>
2017-03-29 14:22:14 +0200

    implemented filter for event search

--- doc/sprint-backlog.md
+++ doc/sprint-backlog.md
@@ -9,7 +9,7 @@
 | Veranstaltung anlegen         |                                  |     1 |      Arthur |
 | Veranstaltung veröffentlichen |                                  |     2 |     Florian |
 | Veranstaltung bearbeiten      |                                  |     3 |      Arthur |
-| Veranstaltung suchen          |                                  |     4 |      Arthur |
+| Veranstaltung suchen          |                                  |     4 |     Florian |
 | Veranstaltung ansehen         |                                  | 5 & 9 |      Arthur |
 | Ticketreservierung            |                                  | 6 & 9 |     Florian |
 | Reservierungsbestätigung      |                                  |     7 |     Florian |
---
+++ src/main/java/controller/EventFilterView.java
@@ -0,0 +1,71 @@
+/*
+ * COPYRIGHT:
+ *
+ * TITLE TO THE CODE REMAIN WITH ZEB/INFORMATION.TECHNOLOGY. THE CODE IS COPYRIGHTED AND PROTECTED BY LAW. YOU WILL NOT
+ * REMOVE ANY COPYRIGHT NOTICE FROM THE CODE. REASSEMBLING, RECOMPILATION, TRANSFER, DISTRIBUTION OR MODIFICATION OF
+ * PART OR ALL OF THE CODE IN ANY FORM WITHOUT THE PRIOR WRITTEN PERMISSION OF ZEB/INFORMATION.TECHNOLOGY IS PROHIBITED.
+ *
+ * created: 29.03.2017 - 13:45:29
+ */
+package controller;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import model.Event;
+import service.EventService;
+
+
+/**
+ * @author fbussmann
+ *
+ */
+@Named
+@ViewScoped
+public class EventFilterView implements Serializable {
+
+    private static final long serialVersionUID = -2666536764493425372L;
+
+    private List<Event>       events;
+
+    private List<Event>       filteredEvents;
+
+    @Inject
+    private EventService      eventService;
+
+    @PostConstruct
+    public void init() {
+        this.events = this.eventService.getPublishedEvents();
+    }
+
+    public List<Event> getEvents() {
+        return this.events;
+    }
+
+    public void setEvents( final List<Event> events ) {
+        this.events = events;
+    }
+
+    public List<Event> getFilteredEvents() {
+        return this.filteredEvents;
+    }
+
+    public void setFilteredEvents( final List<Event> filteredEvents ) {
+        this.filteredEvents = filteredEvents;
+    }
+
+    public EventService getEventService() {
+        return this.eventService;
+    }
+
+    public void setEventService( final EventService eventService ) {
+        this.eventService = eventService;
+    }
+
+}
